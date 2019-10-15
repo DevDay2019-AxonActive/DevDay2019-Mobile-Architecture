30567758a283411b5b8abb4f7b74ea7f814d4ece
Alfie <hieu.ngu@ascendcorp.com>
2019-10-15 19:00:03 +0700

    [wip] add test files

--- v1/Flutter/books/lib/resource/book_api_provider.dart
+++ v1/Flutter/books/lib/resource/book_api_provider.dart
@@ -12,21 +12,43 @@ class BookApiProvider implements Source {
 
   // base url
   final _baseUrl = "https://192.168.70.59:8080/";
+  final _listBookEndpoint = "library-core/api/books/";
   final _searchBookEndpoint = "library-core/api/books/search/";
 
-  /* 
-  This will return a list of books based on the query provided. 
-  It will either return a success or error state.
+/**
+ * This will return a list of books
+ */
+  Future<State> getBooks() async {
+    Response response;
+
+    try {
+      var response = await _client.get('$_baseUrl$_listBookEndpoint');
+      if (response.statusCode == 200)
+        return State<Book>.success(Book.fromJson(json.decode(response.body)));
+      else
+        return State<String>.error(response.statusCode.toString());
+    } finally {
+      _client.close();
+    }
+  }
+
+  /**
+   * This will return a list of books based on the query provided. 
+   * It will either return a success or error state.
    */
-  
+
   Future<State> searchBooksByKeyword(String keyword) async {
     Response response;
 
-    response = await _client.get('$_baseUrl$_searchBookEndpoint');
-
-    if (response.statusCode == 200)
-      return State<Book>.success(Book.fromJson(json.decode(response.body)));
-    else
-      return State<String>.error(response.statusCode.toString());
+    try {
+      var response = await _client
+          .post('$_baseUrl$_searchBookEndpoint', body: {'keyword': keyword});
+      if (response.statusCode == 200)
+        return State<Book>.success(Book.fromJson(json.decode(response.body)));
+      else
+        return State<String>.error(response.statusCode.toString());
+    } finally {
+      _client.close();
+    }
   }
 }
