f61670df3580914557936f7f9887e7ba69f164a9
Alfie <hieu.ngu@ascendcorp.com>
2019-10-15 19:19:51 +0700

    [wip] unit test

--- v1/Flutter/books/test/bloc/login_bloc_test.dart
+++ v1/Flutter/books/test/bloc/login_bloc_test.dart
@@ -1,3 +1,9 @@
-void main () {
+import 'package:books/bloc/login_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  final LoginBloc loginBloc = LoginBloc();
+  test('invalid email', () {
+    
+  });
 }
